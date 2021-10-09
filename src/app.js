22e639a5c3cae2550e9b52546dad58e84b71f600
Augusto Farnese <augustofarnese@gmail.com>
2021-10-09 21:54:02 +0000

    Adding some filters

--- src/app.js
+++ src/app.js
@@ -20,12 +20,56 @@ search.addWidgets([
       item: `
 <article>
   <h1>{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</h1>
+  <p>{{description}}</p>
 </article>
 `,
     },
   }),
   instantsearch.widgets.pagination({
     container: '#pagination',
+  }),
+	  instantsearch.widgets.refinementList({
+    container: '#speaker',
+    attribute: 'speakers',
+  }),
+	  instantsearch.widgets.rangeSlider({
+    container: '#funny_rating',
+    attribute: 'funny_rating',
+  }),
+	  instantsearch.widgets.rangeSlider({
+    container: '#beautiful_rating',
+    attribute: 'beautiful_rating',
+  }),
+	  instantsearch.widgets.refinementList({
+    container: '#tags',
+    attribute: 'tags',
+  }),
+	instantsearch.widgets.clearRefinements({
+  container: '#clear_refinements',
+	}),
+	instantsearch.widgets.rangeSlider({
+    container: '#jaw_droping_rating',
+    attribute: 'jaw_droping_rating',
+  }),
+  instantsearch.widgets.rangeSlider({
+    container: '#inspiring_rating',
+    attribute: 'inspiring_rating',
+  }),
+  instantsearch.widgets.rangeSlider({
+    container: '#ingenious_rating',
+    attribute: 'ingenious_rating',
+  }),
+  instantsearch.widgets.rangeSlider({
+    container: '#informative_rating',
+    attribute: 'informative_rating',
+  }),
+  instantsearch.widgets.rangeSlider({
+    container: '#fascinating_rating',
+    attribute: 'fascinating_rating',
+  }),
+  instantsearch.widgets.rangeSlider({
+    container: '#courageous_rating',
+    attribute: 'courageous_rating',
   }),
 ]);
 
